09e873bcb43076e7fd617d5c30504cabba49fe30
Arbusta <candela.ergas@arbusta.net>
2022-07-25 09:58:35 -0300

    Carpeta+ ejercicios -Cande

---
+++ Ejercicios/Candela Ergas/a.py
@@ -0,0 +1,5 @@
+def sueldo(horas,paga):
+    horas=int(input("Cuantas horas trabajas?"))
+    paga=int(input("Cuánto te pagan por hora?"))
+    total=horas*paga
+    print("Si trabajas",horas,"cobras",total)
