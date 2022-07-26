1cbaa175d4b28d2645b38b9c66488bd251bad0aa
Arbusta <cesar.sanchez@arbusta.net>
2022-07-25 23:45:13 -0300

    Ejercicio corregido

--- Ejercicios/CesarSanchez/Datos Simples/ejercicios.py
+++ Ejercicios/CesarSanchez/Datos Simples/ejercicios.py
@@ -27,7 +27,7 @@ estatura = float (input("Escriba su estatura: "))
 
 calculoMasa = peso / (estatura * estatura)
 
-print ("El IMC es: ", calculoMasa) """
+print ("El IMC es: ", round(calculoMasa,2)) """
 
 
 # 5.- Una panadería vende barras de pan a 3.49€ cada una. El pan que no es el día tiene un descuento del 60%. Escribir un programa que comience leyendo el número de barras vendidas que no son del día. Después el programa debe mostrar el precio habitual de una barra de pan, el descuento que se le hace por no ser fresca y el coste final total
