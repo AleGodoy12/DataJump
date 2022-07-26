a07d22aea662a48f849e8a01636de0b229dfe213
Arbusta <morinigo.matias@arbusta.net>
2022-07-26 13:00:37 -0300

    Evaluacion

---
+++ Evaluacion 26-7/Carpeta 1/Matías Morinigo/Ejercicios.py
@@ -0,0 +1,9 @@
+Cajero = int(input("Ingrese su edad "))
+edadMayor = 18
+if Cajero <=18 and Cajero >=4:
+    print("el precio a pagar es de 5 €")
+else:
+     if Cajero < 4:
+        print("podes ingresar gratis")
+     else:
+        print("el precio a pagar es de 10€")
