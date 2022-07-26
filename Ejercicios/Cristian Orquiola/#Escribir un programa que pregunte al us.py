d2dd8119d15b522704f778f11775ea48891d8400
Arbusta <cristian.orquiola@arbusta.net>
2022-07-26 09:36:54 -0300

    ejercicio 4 subido

---
+++ Ejercicios/Cristian Orquiola/#Escribir un programa que pregunte al us.py
@@ -0,0 +1,7 @@
+#Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora.
+#  Después debe mostrar por pantalla la paga que le corresponde
+
+horas=float(input("¿cuantas horas trabajo?"))
+corresponde=float(input("la paga es"))
+sueldo= horas*corresponde  
+print(sueldo)
---
+++ Ejercicios/Cristian Orquiola/ejercicio4.py
@@ -0,0 +1,4 @@
+horas=float(input("¿cuantas horas trabajo?"))
+corresponde=float(input("la paga es"))
+sueldo= horas*corresponde  
+print(sueldo)
