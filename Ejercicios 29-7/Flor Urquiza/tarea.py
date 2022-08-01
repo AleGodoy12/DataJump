c21b51b98a7054b7842275f839a9fe8a463665cd
Arbusta <florencia.urquiza@arbusta.net>
2022-08-01 15:58:48 -0300

    tarea

---
+++ Ejercicios 29-7/Flor Urquiza/tarea.py
@@ -0,0 +1,47 @@
+#1.- Crea un bucle while que se ejecute hasta que x valga 15 con incrementos de 5
+
+i=0
+while i<= 15 :
+    print (i)
+    i += 5
+    
+#2.- Crea un bucle while que se ejecute hasta que x valga -100 con decrementos de 20.
+
+i=0
+while i>= -100 :
+    print (i)
+    i -= 20
+    
+#3.- Crea un bucle while que se ejecute hasta que x valga 0 con decrementos de 1 y que
+# muestre en cada ejecución esta frase con el valor de ejecución correspondiente: 'El valor del bucle es 10'...
+
+i=10
+while i>= 0 :
+    print ("El valor del bucle es ", i)
+    i -= 1
+    
+#4.- Crea un bucle for que itere la siguiente tupla y muestre una frase como esta
+# en cada iteración: 'El color es: ' + color + '.'
+
+color= ["rosa", "violeta", "azul", "verde"]
+
+for i in colores :
+    print ("El color es:", i, ".")
+    
+
+#5.- Crea un bucle for con un range() que vaya desde el valor 7 hasta el valor 700 en saltos de 100.
+# Basta con que imprimas el valor de cada iteración.
+
+for i in range (7, 800, 100) :
+    print (i)
+    
+
+#**Crea un bucle while con las siguientes características:
+#El valor inicial de la variable x será de 0.                
+#El valor de incremento será 1.                               
+#La condición de salida del bucle será mayor o igual a 30.    
+#La ejecución se deberá romper cuando x valga 15.   break x = 15 
+#Debes imprimir la siguiente frase cada vez que se ejecute el bucle: 'El valor del bucle es: ' + x.
+#Debes saltarte las ejecuciones con valor de x en 4, 6 y 10.
+#En cada salto de ejecución, deberás mostrar los valores saltado con este mensaje: 'Se saltó el valor ' + x ' de x'.
+#Cuando se rompa la ejecución del bucle deberás mostrar un mensaje indicándolo: 'Se rompió la ejecución del bucle cuando x valía ' + x **
--- Ejercicios-22-7/Flor/practicasss.py
+++
@@ -1,4 +0,0 @@
-#3.- Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde
-horasTrabajadas= int(input("Ingrese la cantidad de horas Trabajadas :"))
-costoHora= int(input("Porfavor ingrese costo por hora :"))
-print ("Le corresponde:", (horasTrabajadas*costoHora))
