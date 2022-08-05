938a5679ad9a7075e8b66ea6270b00230b8661ef
Arbusta <jorge.garcia@arbusta.net>
2022-08-05 11:05:56 -0300

    Actualizacion ejercicios

--- Ejercicios 2-8/Jorge García/Ejercicios/ejercicio8.py
+++ Ejercicios 2-8/Jorge García/Ejercicios/ejercicio8.py
@@ -2,23 +2,11 @@
 # (por ejemplo nombre, edad, sexo, teléfono, correo electrónico, etc.) que se le pida al usuario. 
 # Cada vez que se añada un nuevo dato debe imprimirse el contenido del diccionario.
 
-datos = {}
-
-nombre = input("Nombre: ").capitalize()
-datos['Nombre'] = nombre
-print(datos)
-edad = int(input("Edad: "))
-datos['Edad'] = edad 
-print(datos)
-sexo = input("Sexo: ")
-datos['Sexo'] = sexo 
-print(datos)
-direccion = input("Dirección: ").capitalize()
-datos['Direccion'] = direccion 
-print(datos)
-telefono = input("Teléfono: ")
-datos['Telefono'] = telefono 
-print(datos)
-mail = input("Correo electrónico: ")
-datos['Mail'] = mail 
-print(datos)
+respuesta = 0
+persona = {}
+while respuesta != 1:
+    dato = input("Ingresa el dato que quieres agregar: ")
+    valor = input(f"Ingresa tu {dato}: ")
+    persona[dato] = valor
+    print(persona)
+    respuesta = int(input("Si quieres salir ingresa [1], si no ingresa [2]: "))
