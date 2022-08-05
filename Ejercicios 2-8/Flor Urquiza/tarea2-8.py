43dcc5103e134bbcc856985011cb6895d3ebffcf
Arbusta <florencia.urquiza@arbusta.net>
2022-08-05 11:37:25 -0300

    tarea incompleta

--- Ejercicios 2-8/Flor Urquiza/tarea2-8.py
+++ Ejercicios 2-8/Flor Urquiza/tarea2-8.py
@@ -11,3 +11,13 @@ for i in asignaturas :
 #Escribir un programa que pregunte al usuario los números ganadores de la lotería primitiva,
 # los almacene en una lista y los muestre por pantalla ordenados de menor a mayor.
 
+lista= []
+ganadoresLoteria= input("Ingrese los numeros ganadores de la loteria primitiva: ")
+for i in ganadoresLoteria:
+    lista= sorted(ganadoresLoteria)
+
+print(lista)
+
+#6.- Escribir un programa que almacene en una lista los siguientes precios, 
+# 50, 75, 46, 22, 80, 65, 8, y muestre por pantalla el menor y el mayor de los precios.
+
