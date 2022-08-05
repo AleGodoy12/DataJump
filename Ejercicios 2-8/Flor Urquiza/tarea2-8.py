0329103eea3c96cf056a445d8e56b90c306cb9c1
Arbusta <florencia.urquiza@arbusta.net>
2022-08-05 10:13:53 -0300

    tareita dificil

---
+++ Ejercicios 2-8/Flor Urquiza/tarea2-8.py
@@ -0,0 +1,13 @@
+#Escribir un programa que almacene las asignaturas de un curso 
+# (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre
+# por pantalla el mensaje Yo estudio <asignatura>, donde <asignatura> es cada una de las asignaturas de la lista.
+
+asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+
+for i in asignaturas :
+    print ("Yo estudio, ", i)
+
+
+#Escribir un programa que pregunte al usuario los números ganadores de la lotería primitiva,
+# los almacene en una lista y los muestre por pantalla ordenados de menor a mayor.
+
